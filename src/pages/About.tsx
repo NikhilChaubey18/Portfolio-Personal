16f1c6720cdfa3b0b24f656cf0f91fe61456d97a
NikhilChaubey18 <nikhilch1804@gmail.com>
2024-07-21 22:02:03 +0530

    third commit

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -15,7 +15,7 @@ const About = () => {
                     <div className="flex flex-col items-center mt-3">
                         <span className="text-4xl mb-2"><img className="" src="https://media.giphy.com/media/hvRJCLFzcasrR4ia7z/giphy.gif" width="80px" /></span>
                         <span className="text-4xl  mb-2">Education</span>
-                        <span className="text-4xl  mb-2">Basic Qualification</span>
+                     
                         <p className="  text-lg text-center text-gray-600">I actively participate in hackathons and other tech-related activities. Below are some of my major certifications.</p>
                     </div>
                 </div>
